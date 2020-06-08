a6f3a1291b34a558b170cbc5cce6746206157e68
SurekaStoeckigt <sureka.stoeckigt@gmail.com>
2020-06-08 12:40:18 +0100

    Update disemvowel_string.py
    remove all of the vowels from the trolls' comments, neutralizing the threat.
    
    Your task is to write a function that takes a string and return a new string with all vowels removed.
    
    For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
    
    Note: for this kata y isn't considered a vowel.

--- disemvowel_string.py
+++ disemvowel_string.py
@@ -1,9 +1,9 @@
-
-
-Remove all of the vowels from the trolls' comments, neutralizing the threat.
-
-Your task is to write a function that takes a string and return a new string with all vowels removed.
-
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-
-Note: for this kata y isn't considered a vowel.
+def disemvowel(string):
+    bad_chars = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'} 
+    
+    for i in bad_chars : 
+        string = string.replace(i, '')
+    return string
+  
+  test.assert_equals(disemvowel("This website is for losers LOL!"),
+                              "Ths wbst s fr lsrs LL!")
